541ec098ae2628eb33265b11090aff96fe2b8a94
Anastasia Koptilova <koptilova.nastya@gmail.com>
2023-03-17 16:08:46 +0200

    add data

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -11,12 +11,14 @@ import {
 import logo from '../../images/logo.svg';
 import background from '../../images/background.png';
 import avatar from '../../images/avatar.png';
+import users from '../../data/users.json';
 
 export const Card = () => {
   const followersNumber = 100500;
 
   const [follow, setFollow] = useState(false);
   const [followers, setFollowers] = useState(followersNumber);
+  console.log(users);
 
   useEffect(() => {
     const prevState = JSON.parse(localStorage.getItem('card'));
@@ -41,16 +43,38 @@ export const Card = () => {
   };
 
   return (
-    <Container>
-      <img src={background} alt="" />
-      <Logo src={logo} alt="" />
-      <Line></Line>
-      <Avatar src={avatar} alt="" />
-      <Tweets>777 tweets</Tweets>
-      <Followers>{followers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} Followers</Followers>
-      <Button follow={follow} type="button" onClick={handleButton}>
-        {follow ? 'Following' : 'Follow'}
-      </Button>
-    </Container>
+    <div>
+      {users.map(el => {
+        return (
+          <Container key={el.id}>
+            <img src={background} alt="" />
+            <Logo src={logo} alt="" />
+            <Line></Line>
+            <Avatar src={`${el.avatar}`} alt="" />
+            <Tweets>{el.tweets} tweets</Tweets>
+            <Followers>
+              {el.followers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}{' '}
+              Followers
+            </Followers>
+            <Button follow={follow} type="button" onClick={handleButton}>
+              {follow ? 'Following' : 'Follow'}
+            </Button>
+          </Container>
+        );
+      })}
+      {/* <Container>
+        <img src={background} alt="" />
+        <Logo src={logo} alt="" />
+        <Line></Line>
+        <Avatar src={avatar} alt="" />
+        <Tweets>777 tweets</Tweets>
+        <Followers>
+          {followers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} Followers
+        </Followers>
+        <Button follow={follow} type="button" onClick={handleButton}>
+          {follow ? 'Following' : 'Follow'}
+        </Button>
+      </Container> */}
+    </div>
   );
 };
